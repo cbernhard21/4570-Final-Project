312fcd7dceea213164ab65f7ccbfd12d2b7ed81a
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2022-03-04 15:51:52 -0600

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -2,104 +2,108 @@
 
 //function to create 2 arrays, one for short sleeve and on for long sleeve
 function sortProducts(productArr, value, productArr1, productArr2) {
-  productArr.forEach(item => {
-    if (item.sleeve === value) {
-      productArr1.push(item);
-    } else {
-      productArr2.push(item)
-    }
-  });
-}
-
-//function to loop through array and create select and option elements
-function loopProducts(productArr, htmlContainer) {
-  productArr.forEach(shirt => {
-    //create select elements for color and size
-    let shortSleeveColorSelect = document.createElement('select');
-    let shortSleeveSizeSelect = document.createElement('select');
-    let p = document.createElement('p');
-    p.innerText = shirt.style;
-
-    //loop through shirt size and create option for select element
-    shirt.size.forEach(size => {
-      let option = document.createElement('option');
-      option.innerText = size;
-      shortSleeveSizeSelect.appendChild(option);
+    productArr.forEach((item) => {
+        if (item.sleeve === value) {
+            productArr1.push(item);
+        } else {
+            productArr2.push(item);
+        }
     });
-
-    //loop through shirt color and create option for select element
-    shirt.colors.forEach(color => {
-      let option = document.createElement('option');
-      option.innerText = color;
-      shortSleeveColorSelect.appendChild(option);
-    })
-
-    //add all HTML to div to display product details
-    htmlContainer.appendChild(p);
-    htmlContainer.appendChild(shortSleeveColorSelect);
-    htmlContainer.appendChild(shortSleeveSizeSelect);
-  });
-
 }
 
 //function to get product information
 async function getProducts() {
-  const response = await fetch('./products.json');
-  const products = await response.json();
-  return products;
+    const response = await fetch('./products.json');
+    const products = await response.json();
+    return products;
 }
 
 //main function to display all the product information
 async function displayProducts() {
-  //arrays to be filled later
-  let allProducts = [];
-  let menShortSleeve = [];
-  let menLongSleeve = [];
-  let womenShortSleeve = [];
-  let womenLongSleeve = [];
-
-  //DOM elements
-  const shortSleeveContainer = document.querySelector('.short-sleeve-container');
-  const longSleeveContainer = document.querySelector('.long-sleeve-container');
-
-  //create new DOM elements
-  let h3Men1 = document.createElement('h3');
-  h3Men1.innerText = "Men Shirts";
-  let h3Men2 = document.createElement('h3');
-  h3Men2.innerText = "Men Shirts";
-
-  let h3Women1 = document.createElement('h3');
-  h3Women1.innerText = "Women Shirts";
-  let h3Women2 = document.createElement('h3');
-  h3Women2.innerText = "Women Shirts";
+    //arrays to be filled later
+    let allProducts = [];
+    let menShortSleeve = [];
+    let menLongSleeve = [];
+    let womenShortSleeve = [];
+    let womenLongSleeve = [];
+    // let menSizeOptionHtmlShort = '';
+    // let menColorOptionHtmlShort = '';
+    // let menSizeOptionHtmlLong = '';
+    // let menColorOptionHtmlLong = '';
+    // let womenSizeOptionHtmlShort = '';
+    // let womenColorOptionHtmlShort = '';
+    // let womenSizeOptionHtmlLong = '';
+    // let womenColorOptionHtmlLong = '';
+    const menHeading = `<h2>Men's Shirts</h2>`;
+    const womenHeading = `<h2>Women's Shirts</h2>`;
 
 
-  try {
-    allProducts = await getProducts();
-    let menShirts = allProducts.Men;
-    let womenShirts = allProducts.Women;
+    //DOM elements
+    const shortContainer = document.querySelector('.short-sleeve-container');
+    const longContainer = document.querySelector('.long-sleeve-container');
 
-    sortProducts(menShirts, 'short', menShortSleeve, menLongSleeve);
-    sortProducts(womenShirts, 'short', womenShortSleeve, womenLongSleeve)
+    try {
+        allProducts = await getProducts();
+        let menShirts = allProducts.Men;
+        let womenShirts = allProducts.Women;
 
-    //display men shirts
-    shortSleeveContainer.append(h3Men1);
-    loopProducts(menShortSleeve, shortSleeveContainer);
-    longSleeveContainer.append(h3Men2);
-    loopProducts(menLongSleeve, longSleeveContainer);
-
-    //display woman shirts
-    shortSleeveContainer.append(h3Women1);
-    loopProducts(womenShortSleeve, shortSleeveContainer);
-
-    longSleeveContainer.append(h3Women2);
-    loopProducts(womenLongSleeve, longSleeveContainer);
+        //create arrays for men and woman short/long sleeve shirts
+        sortProducts(menShirts, 'short', menShortSleeve, menLongSleeve);
+        sortProducts(womenShirts, 'short', womenShortSleeve, womenLongSleeve);
 
 
-  } catch (e) {
-    console.log('There was an error')
-    console.log(e)
-  }
+        const menShortSleeveHTML = menShortSleeve.map(item => {
+                    return `
+            <form class="card">
+              <h2 class="card-type">${item.type.charAt(0).toUpperCase() + item.type.slice(1)}'s</h2>
+              <h3 class="card-style">${item.style.charAt(0).toUpperCase() + item.style.slice(1)}</h3>
+              <label class="card-label" for="size">Size:</label>
+              <select id="size" class="card-options">
+               ${item.size.map(size => `<option class="size" value="${size}">${size}<option>`).join('')}
+              </select>
+              <label class="card-label" for="color">Color:</label>
+              <select id="color" class="card-options">
+              ${item.colors.map(color => `<option class="color" value="${color}">${color}<option>`).join('')}
+              </select>
+              <input type="submit" class="btn" id="addToCart" value="Add To Cart">
+            </form>
+            `
+        }).join('')
+
+        const womenShortSleeveHTML = womenShortSleeve.map(item => {
+          return `
+            <form class="card">
+              <h2 class="card-type">${item.type.charAt(0).toUpperCase() + item.type.slice(1)}'s</h2>
+              <h3 class="card-style">${item.style.charAt(0).toUpperCase() + item.style.slice(1)}</h3>
+              <label class="card-label" for="size">Size:</label>
+              <select id="size" class="card-options">
+              ${item.size.map(size => `<option value="${size}">${size}<option>`).join('')}
+              </select>
+              <label class="card-label" for="color">Color:</label>
+              <select id="color" class="card-options">
+              ${item.colors.map(color => `<option value="${color}">${color}<option>`).join('')}
+              </select>
+              <input type="submit" class="btn" id="addToCart" value="Add To Cart">
+            </form>
+            `
+          }).join('')
+
+        shortContainer.innerHTML = `<div class="container">${menShortSleeveHTML}</div>
+                                    <div class="container">${womenShortSleeveHTML}</div>`;
+
+        const cartButtons = document.querySelectorAll('.btn');
+
+        cartButtons.forEach(button => {
+         
+          button.addEventListener('click', (e) => {
+            e.preventDefault();
+          })
+        })
+       
+    } catch (e) {
+        console.log('There was an error');
+        console.log(e);
+    }
 }
 
 displayProducts();
--- js/register.js
+++ js/register.js
@@ -1,50 +1,51 @@
 function register() {
-  //global variables
-  const formContainer = document.querySelector('#formBox');
-  const submitButton = document.querySelector('.submit');
-  const errorMessageText = 'Please Fill In All The Fields';
+    //global variables
+    const formContainer = document.querySelector('#formBox');
+    const submitButton = document.querySelector('.submit');
+    const errorMessageText = 'Please Fill In All The Fields';
 
-  //event listeners
-  submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
+    //event listeners
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
 
-    //form values
-    let screenName = document.querySelector('#screenName').value;
-    let username = document.querySelector('#username').value;
-    let password = document.querySelector('#password').value;
-    let errorHtml = document.querySelector('.error');
+        //form values
+        let screenName = document.querySelector('#screenName').value;
+        let username = document.querySelector('#username').value;
+        let password = document.querySelector('#password').value;
+        let errorHtml = document.querySelector('.error');
 
-    //removes error message if it is there
-    if (errorHtml) {
-      errorHtml.remove();
-    }
-
-    //check if username and password are filled in
-    try {
-      if (screenName === '' || username === '' || password === '') {
-        throw err
-      } else {
-        const user = {
-          screenName: screenName,
-          username: username,
-          password: password
+        //removes error message if it is there
+        if (errorHtml) {
+            errorHtml.remove();
         }
-        localStorage.setItem('user', JSON.stringify(user));
-        sessionStorage.setItem('isLoggedIn', true);
-        window.location.pathname = '/profile.html';
-      }
-    } catch (err) {
-      addErrorMessage(formContainer, errorMessageText);
-    }
-  });
+
+        //check if username and password are filled in
+        try {
+            if (screenName === '' || username === '' || password === '') {
+                throw err
+            } else {
+                const user = {
+                    screenName: screenName,
+                    username: username,
+                    password: password,
+                    cart: []
+                }
+                localStorage.setItem('user', JSON.stringify(user));
+                sessionStorage.setItem('isLoggedIn', true);
+                window.location.pathname = '/profile.html';
+            }
+        } catch (err) {
+            addErrorMessage(formContainer, errorMessageText);
+        }
+    });
 }
 
 //function to add error message to DOM, takes in a HTML container to insert the text and a message
 const addErrorMessage = (htmlContainer, message) => {
-  let errorMessage = document.createElement('p');
-  errorMessage.textContent = message;
-  errorMessage.className = 'error';
-  htmlContainer.appendChild(errorMessage);
+    let errorMessage = document.createElement('p');
+    errorMessage.textContent = message;
+    errorMessage.className = 'error';
+    htmlContainer.appendChild(errorMessage);
 }
 
 register();
